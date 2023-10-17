c65303a5922f7093ebf34bfa691afdddc4356f64
Alejandro Zamora <alex_speed_95@hotmail.com>
2023-10-16 19:03:29 -0700

    created AlertContext and Reducer

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import { GithubProvider } from "./context/github/githubContext";
+import { AlertProvider } from "./context/alerts/alertContext";
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route exact path="/" element={<Home />}></Route>
-              <Route path="/about" element={<About />}></Route>
-              <Route path="/notfound" element={<NotFound />}></Route>
-              <Route path="/*" element={<NotFound />}></Route>
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route exact path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/notfound" element={<NotFound />}></Route>
+                <Route path="/*" element={<NotFound />}></Route>
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
---
+++ src/context/alerts/alertContext.js
@@ -0,0 +1,27 @@
+import { createContext, useReducer } from "react";
+import alertReducer from "./alertReducer";
+
+const AlertContext = createContext();
+
+export const AlertProvider = ({ children }) => {
+  const initialState = null;
+
+  const [state, dispatch] = useReducer(alertReducer, initialState);
+
+  //Set an Alert
+  const setAlert = (message, type) => {
+    dispatch({
+      type: "SET_ALERT",
+      payload: { message, type },
+    });
+    setTimeout(() => dispatch({ type: "REMOVE_ALERT" }), 3000);
+  };
+
+  return (
+    <AlertContext.Provider value={{ alert: state, setAlert }}>
+      {children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertContext;
