5fd39cdbaebb3c875383e32aa5fc359c0874ec7b
Alejandro Zamora <alex_speed_95@hotmail.com>
2023-10-16 19:16:20 -0700

    Alert Component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import { GithubProvider } from "./context/github/githubContext";
 import { AlertProvider } from "./context/alerts/alertContext";
+import Alert from "./components/layout/Alert";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <div className="flex flex-col justify-between h-screen">
             <Navbar />
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
               <Routes>
                 <Route exact path="/" element={<Home />}></Route>
                 <Route path="/about" element={<About />}></Route>
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/githubContext";
+import AlertContext from "../../context/alerts/alertContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -14,7 +16,7 @@ function UserSearch() {
     e.preventDefault();
 
     if (text === "") {
-      alert("Please enter something");
+      setAlert("Please enter something", "error");
     } else {
       searchUsers(text);
       setText("");
